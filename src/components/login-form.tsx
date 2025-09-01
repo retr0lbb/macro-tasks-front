5010d1ab049e2f0551e0a7a79da9cc3e28b742a2
retr0lbb <retr0lbb@gmail.com>
2025-09-01 02:45:30 -0300

    feat: add form to login page

---
+++ src/components/login-form.tsx
@@ -0,0 +1,74 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import z from "zod/v4";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+const loginFormSchema = z.object({
+  email: z.email(),
+  password: z.string().min(6).max(64),
+});
+
+export function LoginForm() {
+  const loginForm = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof loginFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+    loginForm.reset();
+  }
+
+  return (
+    <Form {...loginForm}>
+      <form onSubmit={loginForm.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={loginForm.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={loginForm.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Senha</FormLabel>
+              <FormControl>
+                <Input placeholder="Senha" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button size={"lg"} variant="default" className="rounded-md">
+          Entrar
+        </Button>
+      </form>
+    </Form>
+  );
+}
--- src/routes/(auth)/login.tsx
+++ src/routes/(auth)/login.tsx
@@ -1,9 +1,58 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { LoginForm } from "@/components/login-form";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { Chrome, Github, ArrowLeft } from "lucide-react";
 
 export const Route = createFileRoute("/(auth)/login")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <main className="min-h-dvh overflow-hidden">a</main>;
+  return (
+    <main className="min-h-dvh overflow-hidden grid grid-cols-5 place-items-center">
+      <div className="w-full h-full p-5 col-span-2 bg-violet-700 relative">
+        <div className="absolute top-10 left-10 p-2 flex items-center justify-center text-zinc-300">
+          <Link to="/" className="flex items-center justify-center gap-2">
+            <ArrowLeft /> Voltar
+          </Link>
+        </div>
+        <Card className="w-full h-full flex flex-col justify-center px-10">
+          <CardHeader className="flex flex-col gap-2">
+            <CardTitle>
+              <h1 className="text-4xl font-bold text-zinc-200">Login</h1>
+            </CardTitle>
+            <CardAction className="flex items-center justify-center gap-1 text-zinc-400">
+              Não tem uma conta?{" "}
+              <span className="text-lg underline cursor-pointer hover:text-zinc-200">
+                Cadastre-se
+              </span>
+            </CardAction>
+          </CardHeader>
+          <CardContent>
+            <LoginForm />
+            <Separator className="mt-5 mb-1" />
+          </CardContent>
+          <CardFooter className="flex gap-2 items-center">
+            <Button variant={"outline"}>
+              <Chrome /> Login com Google
+            </Button>
+            <Separator orientation="vertical" />
+            <Button variant={"outline"}>
+              <Github /> Login com Github
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="bg-violet-700 w-full h-full col-span-3"></div>
+    </main>
+  );
 }
