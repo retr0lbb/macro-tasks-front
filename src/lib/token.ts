8081a2a2f92a3b56085f03a12b29513438a0cdd2
retr0lbb <retr0lbb@gmail.com>
2025-10-24 03:16:08 -0300

    feat: add https to load the tokens

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -8,10 +8,11 @@ export function getToken(): string{
     const token = sessionStorage.getItem("@HYPERBOLIC_TASKS:csrf_token")
 
     if(!token){
-        throw new Error("Token not found")
+        // throw new Error("Token not found")
+        console.log(token)
     }
 
-    return token
+    return token ?? ""
 }
 
 export function clearToken(){
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import fs from "fs"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,6 +18,14 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
+    }
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "certs/localhost-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost.pem"))
+      
+
     }
   }
 })
